f4bf8006c3da8e2bcf685b1e1b2538f10f48000f
rahila sana <sanakhan@email.com>
2026-08-11 09:20:05 +0500

    configure project for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/pizza_house",
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
